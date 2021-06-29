af4664b6a03f7aa56cda0543dd7a1df3ad909417
dpedrero <el_maldito_pit@hotmail.com>
2021-06-29 22:09:43 +0000

    Curso4.1

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -5,11 +5,15 @@ sap.ui.define([
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
 	function (Controller) {
-		"use strict";
+		"use strict"; //Para comprobar los errores de javascript
 
 		return Controller.extend("logaligroup.invoices.controller.MainView", {
 			onInit: function () {
-
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("./model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen");
+                //this.getView().setModel(oJSONModel, "selectionScreen");
 			}
 		});
 	});
